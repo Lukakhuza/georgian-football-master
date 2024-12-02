77ea1a89c6957d595ab801cd7ca55c28ff5e4648
lukakhuza <lukakhuza7@gmail.com>
2024-12-02 07:08:48 -0500

    test image added to the modal

--- src/components/StadiumModal.jsx
+++ src/components/StadiumModal.jsx
@@ -7,7 +7,21 @@ export default function StadiumModal({ children }) {
   return createPortal(
     <>
       <div className={classes["backdrop"]} />
-      <motion.dialog open className={classes["modal"]}>
+      <motion.dialog
+        open
+        className={classes["modal"]}
+        variants={{
+          hidden: { opacity: 0, y: 30 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
+      >
+        <img
+          src="https://geofootball.s3.us-east-1.amazonaws.com/stadiums/batumi-arena.png"
+          alt="Batumi Arena"
+        />
         {children}
       </motion.dialog>
     </>,
